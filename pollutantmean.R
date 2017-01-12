3fcc7bfabbfbbfa28923cf7dd2b8def5820083ef
elktest <erin.stratton@gmail.com>
2017-01-12 16:12:38 -0500

    Add files via upload

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,38 +1,13 @@
-## Combine CSV files into one dataframe by listing all the files in the directory
-
-pollutantmean <- function(directory, pollutant, id = 1:332) {
-        directory <- "C:/Users/erinstratton/Documents/Programming/Coursera/Assignments/specdata/"
-        listfiles <- as.character(list.files(directory))
-        path <- paste0(directory,listfiles)
-        readfile <- data.frame()
-        
-        ##filename <- c(paste0("00",id[1:9],".csv"),
-        ##              paste0("0",id[10:99],".csv"),
-        ##             paste0(id[100:length(id)],".csv"))
-        
-        
-        for(i in id) {
-                readfile <- rbind(readfile, read.csv(path[i]))
-        }
-        summary(readfile)
-}
-
-
 
 ## Assignment 1
 
 pollutantmean <- function(directory, pollutant, id = 1:332) {
 
-        ## Set directory
-        directory <- "C:/Users/erinstratton/Documents/Programming/Coursera/Assignments/specdata/"
-        
-        ## Define pollutant
-        
-        ## Create character vector with each filename based on length of id
-        filename <- as.character(list.files(directory)) ##paste0(formatC(id, width=3, flag="0"),".csv")
+        ## Create character vector with each filename in folder
+        filename <- as.character(list.files(directory)) 
         
         ## Create character vector with full filepath
-        path <- as.character(paste0(directory,filename))
+        path <- as.character(paste0(directory,"/",filename))
         
         ## Initialize empty data frame
         pollutantdata <- data.frame()
@@ -40,16 +15,12 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
         ## Create for loop to add rows of each file to data frame
         for(i in id) {
                 pollutantdata <- rbind(pollutantdata, read.csv((path[i]),
-                header=TRUE, sep=",", 
-                colClasses=c("Date","numeric","numeric","numeric"),
-                stringsAsFactors=FALSE))        
-                meancal <- mean(pollutantdata[,pollutant],na.rm=TRUE)
-
-        }
-        meancal
+                header=TRUE, sep=","))
         
         ## Calculate the mean of pollutant after removing NA values
-
+                meancol <- mean(pollutantdata[,pollutant],na.rm=TRUE)
+        }
+        meancol
 }
 
- 
+
